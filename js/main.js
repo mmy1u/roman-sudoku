3e71f357f8ddba872d00455e13815912aa66954b
mmy1u <mahady999979@gmail.com>
2026-08-26 06:43:54 +0400

    update js/main.js

--- js/main.js
+++ js/main.js
@@ -5,5 +5,9 @@ const board = document.getElementById("board");
 for (let i = 0; i < 81; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
+
+    const randomNumber = Math.floor(Math.random() * 9);
+    cell.textContent = romanNumerals[randomNumber];
+
     board.appendChild(cell);
 }
