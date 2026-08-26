b6bfff9befec8e4836cdecfa7b93a676e30a3f38
mmy1u <mahady999979@gmail.com>
2026-08-26 06:57:14 +0400

    update js/main.js

--- js/main.js
+++ js/main.js
@@ -36,7 +36,31 @@ function isValid(grid, row, col, num) {
     // اجتاز كل الفحوصات = الرقم مسموح
     return true;
 }
+function fillGrid(grid) {
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
 
+            if (grid[row][col] === 0) {
+
+                for (let num = 1; num <= 9; num++) {
+
+                    if (isValid(grid, row, col, num)) {
+                        grid[row][col] = num;
+
+                        if (fillGrid(grid)) {
+                            return true;
+                        }
+
+                        grid[row][col] = 0;
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+    return true;
+}
 const sudokuGrid = createEmptyGrid();
 console.log(sudokuGrid);
 
